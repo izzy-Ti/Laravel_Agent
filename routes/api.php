14f35b30d7131beae1f17894a5c35fd48251aff5
izzy-Ti <israelashenafi29@gmail.com>
2026-08-23 15:08:16 -0700

    FIX: Route structure fixed

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
-
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AgentToolController;
 
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('/agent/check-availability', [AgentToolController::class, 'checkAvailability']);
-    Route::post('/agent/create-booking', [AgentToolController::class, 'createBooking']);
-});
+Route::post('/agent/check-availability', [AgentToolController::class, 'checkAvailability']);
+Route::post('/agent/create-booking', [AgentToolController::class, 'createBooking']);
